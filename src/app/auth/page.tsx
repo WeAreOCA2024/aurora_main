e394abb84cc0393341947bb630d0c6744db8ebeb
kmjak <tymkoron1221@icloud.com>
2024-09-05 16:31:41 +0900

    topページのheaderの完成

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,29 @@
+import Link from 'next/link';
+
+export default function Page() {
+  const headerOptions = (name:string,link:string) => {
+    return (
+      <Link href={link}>
+        <li className="relative text-base cursor-pointer group px-2 text-center">
+          {name}
+          <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-red-300 transition-all duration-700 group-hover:w-full mt-3"></span>
+        </li>
+      </Link>
+    )
+
+  }
+  return (
+    <>
+      <header className="flex justify-around w-1/2 mx-[calc(100%/4)] py-4 items-end">
+        <h1 className="text-5xl">Aurora</h1>
+        <ul className="flex gap-3">
+          {headerOptions("プロダクト","/product")}
+          {headerOptions("料金","/price")}
+          {headerOptions("問い合わせ","/contact")}
+        </ul>
+      </header>
+      <main className="flex justify-center pt-96 gap-12">
+      </main>
+    </>
+  );
+}

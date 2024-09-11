c60756138aed05caf2fc5c05636feb3cc58b2dee
kmjak <tymkoron1221@icloud.com>
2024-09-11 11:03:49 +0900

    Auth.js

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth"
 import Cognito from "next-auth/providers/cognito"
  
-export const { handlers, auth, signIn, signOut } = NextAuth({
+export const { handlers: { GET, POST }, auth, signIn, signOut } = NextAuth({
   providers: [Cognito],
 })
